7811fa4ba70abb5a6d695efa87f611e32080546e
Nikegamerjjjj <69197950+Nikegamerjjjj@users.noreply.github.com>
2021-05-04 19:11:19 +0200

    Update Cooking Game.py

--- Cooking Game.py
+++ Cooking Game.py
@@ -2,9 +2,63 @@ print("Loading all needed things, please show patient :)")
 import random#temporally
 print("...")
 import time
+def print_pause():
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print
 water_enough = False
 potato_enough = False
 carrot_enough = False
+coins = 0
 water = 120 #desiliters
 potato = 1000 #grams
 carrot = 12 #12 carrots
@@ -12,7 +66,7 @@ meat_packages = 0 # going to be 20 meat packages
 meat_achivement = False
 soup = 0
 time.sleep(2)
-print("Hello and welcome to Cooking Game! v.0.3.03")
+print("Hello and welcome to Cooking Game! v.0.4: Market update and Public release!")
 input("click ENTER to continue...")
 print("What is your name or in this case nickname?")
 username = input("My nickname is... ")
@@ -39,13 +93,16 @@ while True:
         potato = 0
     if carrot < 0:
         carrot = 0
-    if meat_achivement == False:
-        if soup == 1:
-            meat_achivement = True
-            print("You got Meat achievement meaning you can have meat, in inventory")
-            meat_packages += 20
+    if coins < 0:
+        coins = 0
+    #if meat_achivement == False:
+    #   if soup == 1:
+    #        meat_achivement = True
+    #       print("You got Meat achievement meaning you can have meat, in inventory")
+    #        meat_packages += 20
 
     if "recipe" in command:
+        print_pause()
         print("                   Current Recipes:")
         print("                   ")
         print("                   Soup:")
@@ -53,14 +110,17 @@ while True:
         print("                         Potato: 200 grams")
         print("                         Carrots: 3")
     if "inventory" in command:
+        print_pause()
         print("                   You have ", water, " desiliters of water")
         print("                   You have ", potato, " grams of potato")
         print("                   You have ", carrot, " carrots")
         print("                   You have ", soup, "soup")
-        if meat_achivement == True:
-                    print("                   You have ", meat_packages, "meat packages")
+        print("                   You have ", coins, "coins")
+        #if meat_achivement == True:
+        #           print("                   You have ", meat_packages, "meat packages")
         print("                   Tip: If you have small amount of required amount of ingredients, you won't be able to make food!")
     if "make something" in command:
+        print_pause()
         print("                   Write name of any food above:")
         print("                   Soup")
         make_something = input("                   ")
@@ -82,8 +142,7 @@ while True:
                 print("Yeah! All ingredients is here! You succesfully made some Soup!")
                 print("- You got 1 Soup! It lies in inventory now -")
                 soup = soup + 1
-                water
-            elif water_enough == False or potato_enough == False or carrot_enough == False:
+            else:
                 print("You can't make soup, because you dont have enough ingredients!")
                 print("You have " + water + ", instead of 10 desiliters!")
                 print("You have " + potato + ", instead of 200 grams!")
@@ -92,6 +151,7 @@ while True:
             potato_enough = False
             carrot_enough = False
     if "market" in command:
+        print_pause()
         print("Welcome to The Market!")
         time.sleep(2)
         print("Here, can you buy and sell things, such like food.")
@@ -105,11 +165,63 @@ while True:
         market_commands = input()
         if "exit" in market_commands:
             continue
-        if "Sell" in market_commands:
-            print("You can sell only food you made.")
-            print("You have ", soup, " to sell.")
-            print("If you want to sell write: Sell and name of food you want to sell.")
-    else: 
+        if "sell" in market_commands:
+            if soup < 0:
+                print("You dont have anything to sell, redirecting back in 3 sec")
+                time.sleep(3)
+                continue
+            elif soup > 0:
+                print("You can sell only food you made.")
+                time.sleep(2)
+                print("Prices on foods change after time(update), so be in right time to get much coins!")
+                time.sleep(3)
+                print("You can sell ", soup, " soup.")
+                time.sleep(2)
+                print("If you want to sell write: name of food you want to sell.")
+                sell_command = input()
+                if "soup" in sell_command:
+                    print("One soup costs: 25 coins")
+                    print("Want to sell 1 one soup for current price? If not, you will be redirected back to main commands!")
+                    sell_soup_answer = input("Write yes or no...")
+                    if "yes" in sell_soup_answer:
+                        print("Selling soup!")
+                        time.sleep(2)
+                        print("You got 25 coins")
+                        coins += 25
+        if "buy" in market_commands:
+            if coins == 0:
+                print("You dont have enough money! Leaving Market in 2 sec.")
+                time.sleep(2)
+                continue
+            else:
+                print("You can buy vegetables, water and other!")
+                time.sleep(2)
+                print("Here is what you can buy:")
+                print("Water, 20 desiliters: 10 coins")
+                print("Potato, 400 grams: 5 coins")
+                print("Carrots, 6: 7 coins")
+                time.sleep(3)
+                print("Write in commands below what you want to buy:")
+                print("Water")
+                print("Potato")
+                print("Carrot")
+                buy_commands = input()
+                if "water" in buy_commands:
+                    print("Adding 20 desiliters, minus 10 coins")
+                    water += 20
+                    coins -= 10
+                elif "potato" in buy_commands:
+                    print("Adding 400 grams potatoes, minus 5 coins")
+                    potato += 400
+                    coins -= 5
+                elif "carrot" in buy_commands:
+                    print("Adding 6 carrots, minus 7 coins")
+                    carrot += 6
+                    coins -= 7
+
+
+
+    else:
         continue 
 
 
